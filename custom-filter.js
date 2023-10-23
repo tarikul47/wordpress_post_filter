e2300603b0e62c7195518499461ce9d8f260156b
Tarikul Islam <34490496+tarikul47@users.noreply.github.com>
2023-10-23 21:43:08 +0600

    Update custom-filter.js

--- custom-filter.js
+++ custom-filter.js
@@ -2,17 +2,15 @@ jQuery(document).ready(function ($) {
   $("#category-filter-form").on("submit", function (e) {
     e.preventDefault();
 
-    console.log('clikc');
-
     var filterData = $(this).serialize();
 
-    console.log(custom_filter_params);
     $.ajax({
       type: "POST",
       url: custom_filter_params.ajax_url,
       data: filterData,
       success: function (response) {
         $("#filtered-posts").html(response);
+        console.log("response", response);
       },
     });
   });
